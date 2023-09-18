9e6b5d3c201ef1662ea635671b933e0cfae1d5be
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2023-09-18 10:13:27 -0400

    Login
    Tried but failed so far changing the login too logout.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 import * as Model from "../model/model.js"
 
+import { setLoggedInOut } from "../model/model.js";
+
 function initURLListener() {
 $(window).on('hashchange', Model.changeRoute);
 Model.changeRoute();
@@ -8,3 +10,17 @@ Model.changeRoute();
 $(document).ready(function () {
 initURLListener();
 });
+
+// Login/out
+
+function initListeners() {
+    $("nav .anchors .login").on("click", (e) => {
+        e.preventDefault();
+        let btnID = e.currentTarget.id;
+        console.log(btnID);
+        addPageContent(btnID);
+    });
+    $("nav .anchors .login").on("click", (e) => {
+        e.preventDefault();        setLoggedInOut();
+    })
+}
--- model/model.js
+++ model/model.js
@@ -1,5 +1,7 @@
 export var wordArray = ["home", "about", "flavors"];
 
+// Url Change
+
 export function changeRoute() {
     let hashTag = window.location.hash;
     let pageID = hashTag.replace('#', '');
@@ -16,4 +18,23 @@ export function changeRoute() {
      $('#app').html(data);
     });
     }
+}
+
+// Login/Out
+
+var loggedInOut = false;
+
+
+export function setLoggedInOut(){
+    if (loggedInOut == true){
+        loggedInOut = false;
+        $("nav .anchors .login").html("Log In");
+    }else {
+        loggedInOut = true;
+        $("nav .anchors .login").html("Log Out");
     }
+}
+
+export function getLoggedInOut() {
+    return loggedIn;
+}
